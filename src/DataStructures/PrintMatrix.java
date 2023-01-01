042eb38232ead166af039c8e0eb6d66873a2ef87
IvelinTapalilovQA <119358506+IvelinTapalilovQA@users.noreply.github.com>
2023-01-01 23:13:50 +0200

    Data Structures

---
+++ src/DataStructures/PrintMatrix.java
@@ -0,0 +1,31 @@
+package DataStructures;
+
+import java.util.Scanner;
+
+public class PrintMatrix {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter row: ");
+        int rows = scanner.nextInt();
+
+        System.out.println("Enter column: ");
+        int columns = scanner.nextInt();
+
+        int[][] matrix = new int[rows][columns];
+
+        for (int i = 0; i < rows; i++) {
+            System.out.println("Row number is: " + (i + 1));
+            for (int j = 0; j < columns; j++) {
+                System.out.println("Enter digit: ");
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+        System.out.println();
+        }
+    }
+}
