0e645ddbc59bf1bc52c0b81ce09eb69c829b3e5e
IvelinTapalilovQA <119358506+IvelinTapalilovQA@users.noreply.github.com>
2023-01-01 21:36:00 +0200

    Commit loops

---
+++ src/DataStructures/SumOfArray.java
@@ -0,0 +1,25 @@
+package DataStructures;
+
+import java.util.Scanner;
+
+public class SumOfArray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter size: ");
+        int arraySize = scanner.nextInt();
+        int[] arrayOfNumbers = new int[arraySize];
+        int sumOfArray = 0;
+
+
+        for (int j = 0; j < arrayOfNumbers.length; j++) {
+            System.out.println("Enter digit: ");
+            arrayOfNumbers[j] = scanner.nextInt();
+        }
+
+        for (int currentArrayNumber : arrayOfNumbers) {
+            sumOfArray += currentArrayNumber;
+        }
+    System.out.println(sumOfArray);
+    }
+}
