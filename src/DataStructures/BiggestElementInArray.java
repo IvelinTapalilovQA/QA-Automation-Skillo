dfdb734b5fa98971569e8bd2c2f1ee8a89854231
IvelinTapalilovQA <119358506+IvelinTapalilovQA@users.noreply.github.com>
2023-01-02 23:27:04 +0200

    Data Structures

---
+++ src/DataStructures/BiggestElementInArray.java
@@ -0,0 +1,25 @@
+package DataStructures;
+
+import java.util.Scanner;
+
+public class BiggestElementInArray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the size of array: ");
+        int arraySize = scanner.nextInt();
+        int[] arrayOfNumbers = new int[arraySize];
+
+        for (int i = 0; i < arrayOfNumbers.length; i++) {
+            System.out.println("Enter number: ");
+            arrayOfNumbers[i] = scanner.nextInt();
+        }
+        int maxNumber = Integer.MIN_VALUE;
+        for (int currentNumber : arrayOfNumbers) {
+            if (currentNumber > maxNumber) {
+                maxNumber = currentNumber;
+            }
+        }
+    System.out.println("The biggest number in the array is: " + maxNumber);
+    }
+}
