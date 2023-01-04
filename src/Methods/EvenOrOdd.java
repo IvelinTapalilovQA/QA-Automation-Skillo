c5d3572610f9353fc141237c843389dcceec4be0
IvelinTapalilovQA <119358506+IvelinTapalilovQA@users.noreply.github.com>
2023-01-04 22:55:12 +0200

    Methods

---
+++ src/Methods/EvenOrOdd.java
@@ -0,0 +1,18 @@
+package Methods;
+
+import java.util.Scanner;
+
+public class EvenOrOdd {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter number: ");
+        int number = scanner.nextInt();
+
+        boolean result = isEven(number);
+        System.out.println("Is number " + number + " even? - " + result);
+    }
+    public static boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+}
