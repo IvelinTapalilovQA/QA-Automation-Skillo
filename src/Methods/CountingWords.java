b31d25ff767f13870bf166c3aa5249fb374d9664
IvelinTapalilovQA <119358506+IvelinTapalilovQA@users.noreply.github.com>
2023-01-04 02:29:34 +0200

    Methods

---
+++ src/Methods/CountingWords.java
@@ -0,0 +1,21 @@
+package Methods;
+
+import java.util.Scanner;
+
+public class CountingWords {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter sentence: ");
+        String sentence = scanner.nextLine().trim();
+
+        int result = stringCounter(sentence);
+
+        System.out.println(result);
+    }
+    public static int stringCounter(String sentence) {
+
+        return sentence.split(" ").length;
+    }
+}
+
