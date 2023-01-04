69e27a59d7712e446d341e791475871b84722e9c
IvelinTapalilovQA <119358506+IvelinTapalilovQA@users.noreply.github.com>
2023-01-04 23:12:18 +0200

    Methods

---
+++ src/Methods/ReverseNumber.java
@@ -0,0 +1,24 @@
+package Methods;
+
+import java.util.Scanner;
+
+public class ReverseNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = scanner.nextInt();
+
+        int result = reverseString(number);
+        System.out.println(result);
+    }
+    public static int reverseString(int number){
+        int reversedNumber = 0;
+
+        while (number != 0){
+           int lastNumber = number % 10;
+           reversedNumber = (reversedNumber * 10) + lastNumber;
+           number = number / 10;
+        }
+    return reversedNumber;
+    }
+}
